8026d0de8270a49841906c057b3a56873204c944
AnaRurato <ana.rurato.fernandes@escola.pr.gov.br>
2024-11-07 09:20:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js - Pode ser usado para adicionar interatividade no futuro
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("O site de Carlo Acutis está pronto!");
+});
